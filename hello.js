131d269e71b14a4bb018be113df711d00a985588
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-09-13 17:52:15 +0600

    tostring,lower,upper case commit

--- hello.js
+++ hello.js
@@ -1,9 +1,24 @@
-var bananaPrice = 12;
-console.log(bananaPrice);
-var seenAfter = 21;
-console.log(typeof seenAfter)
-var SirName = 'TeacherHighSchool JamsheedMojumdar';
-console.log(typeof SirName)
-var isHot = true;
-var isCool = false;
-console.log(typeof isCool)
+// var bananaPrice = 12;
+// console.log(bananaPrice);
+// var seenAfter = 21;
+// console.log(typeof seenAfter)
+// var SirName = 'TeacherHighSchool JamsheedMojumdar';
+// console.log(typeof SirName)
+// var isHot = true;
+// var isCool = false;
+// console.log(typeof isCool)
+
+
+
+
+// lowerCase, upper case and tosting
+var promise = "I will do HARD work, i will be success";
+console.log(promise.toLowerCase())
+
+
+var promise = "I will do HARD work, i will be success";
+console.log(promise.toUpperCase())
+
+
+var promise = "I will do hard work  22, i WILL be  success 1 day";
+console.log(promise.toString())
