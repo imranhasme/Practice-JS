2d0fe8f18ea58571dc5147268016760dfc2ca2c4
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-09-13 11:51:56 +0600

    secound commit

---
+++ hello.js
@@ -0,0 +1,9 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+var seenAfter = 21;
+console.log(typeof seenAfter)
+var SirName = 'TeacherHighSchool JamsheedMojumdar';
+console.log(typeof SirName)
+var isHot = true;
+var isCool = false;
+console.log(typeof isCool)
