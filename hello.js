c0fdd9a09161004e12b885c5292eeb99b1a1eba0
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-09-15 09:42:31 +0600

    sending split commit

--- hello.js
+++ hello.js
@@ -1,30 +1,30 @@
-// var bananaPrice = 12;
-// console.log(bananaPrice);
-// var seenAfter = 21;
-// console.log(typeof seenAfter)
-// var SirName = 'TeacherHighSchool JamsheedMojumdar';
-// console.log(typeof SirName)
-// var isHot = true;
-// var isCool = false;
-// console.log(typeof isCool)
+var bananaPrice = 12;
+ console.log(bananaPrice);
+ var seenAfter = 21;
+console.log(typeof seenAfter)
+var SirName = 'TeacherHighSchool JamsheedMojumdar';
+ console.log(typeof SirName)
+ var isHot = true;
+var isCool = false;
+ console.log(typeof isCool)
 
 
 
 
 // lowerCase, upper case and toString
 
-// var promise = "I will do HARD work, i will be success";
-// console.log(promise.toUpperCase())
+ var promise = "I will do HARD work, i will be success";
+ console.log(promise.toUpperCase())
 
 
-// var promise = "I will do hard work  22, i WILL be  success 1 day";
-// console.log(promise.toString())
+ var promise = "I will do hard work  22, i WILL be  success 1 day";
+ console.log(promise.toString())
 
 
 
-// // when use indexof
-// var promise = "I will do hard work , i WILL be  success one day";
-// console.log(promise.indexOf('one'))
+// when use indexof
+ var promise = "I will do hard work , i WILL be  success one day";
+ console.log(promise.indexOf('one'))
 
 
 
@@ -45,11 +45,14 @@ myName = 'Rahman';
 
 // i can in call any letter to String [indexof('which is i call now')]//
 
-// var promise = "I am the person, Who never success in any exam  ";
-// console.log(promise.indexOf('in'));
-// // when we write a word same we will find the call of letter ! but when we are write not same ! then result will -1
-// var promise = "I am the person, Who never success in any exam ";
-// console.log(promise.indexOf('In'));
+ var promise = "I am the person, Who never success in any exam  ";
+ console.log(promise.indexOf('in'));
+
+
+
+// when we write a word same we will find the call of letter ! but when we are write not same ! then result will -1
+var promise = "I am the person, Who never success in any exam ";
+console.log(promise.indexOf('In'));
 
 
 
@@ -59,4 +62,3 @@ myName = 'Rahman';
 //split is use for cut out of letter!
 var promise = "how are you Imran? I am fine also."
 console.log(promise.split(' '))
-//
