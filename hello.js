b8f504f7422fa587ac21d1691240a202b8a9ec0e
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-09-15 08:34:13 +0600

    first commit

--- hello.js
+++ hello.js
@@ -13,15 +13,40 @@
 
 // lowerCase, upper case and toString
 
-var promise = "I will do HARD work, i will be success";
-console.log(promise.toUpperCase())
+// var promise = "I will do HARD work, i will be success";
+// console.log(promise.toUpperCase())
 
 
-var promise = "I will do hard work  22, i WILL be  success 1 day";
-console.log(promise.toString())
+// var promise = "I will do hard work  22, i WILL be  success 1 day";
+// console.log(promise.toString())
 
 
 
-// when use indexof
-var promise = "I will do hard work , i WILL be  success one day";
-console.log(promise.indexOf('one'))
+// // when use indexof
+// var promise = "I will do hard work , i WILL be  success one day";
+// console.log(promise.indexOf('one'))
+
+
+
+
+
+
+
+
+// variable name call types//
+var myName = 'Bongo';
+myName = 'other name can i declear in it- Bondhu';
+myName = 'Sheikh';
+myName = 'Mojibur';
+myName = 'Rahman';
+
+
+
+
+// i can in call any letter to String [indexof('which is i call now')]//
+
+var promise = "I am the person, Who never success in any exam  ";
+console.log(promise.indexOf('in'));
+// when we write a word same we will find the call of letter ! but when we are write not same ! then result will -1
+var promise = "I am the person, Who never success in any exam ";
+console.log(promise.indexOf('In'));
