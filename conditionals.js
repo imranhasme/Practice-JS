6a684e6e83423ca7d51f9d1a4a38f6505ed7382b
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-09-23 11:48:56 +0600

    conditionals of js commit

---
+++ conditionals.js
@@ -0,0 +1,24 @@
+// &&-------2 condition must be fullfill
+// ||-----------when 1 conditon will be run
+
+
+var jobPaiso = true;
+var savingAmount = 50000;
+
+
+if(jobPaiso == true && savingAmount > 200000){
+console.log("Aso tomar patri khuji");
+}
+else if(jobPaiso == true){
+    console.log("koyek din por theke dekhbo")
+}
+else{
+    console.log("Tor kopale biye nai");
+
+}
+
+
+
+
+
+
---
+++ explore.js
@@ -0,0 +1,3 @@
+//js time------------
+var date = new date('1971-9-22');
+console.log(date);
