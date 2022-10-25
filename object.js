407ee6a5dd4fccc943a75528d993b5b7a9e9ae9d
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-25 20:50:12 +0600

    simple object commit

---
+++ object.js
@@ -0,0 +1,8 @@
+//simple way to call any object element-----------------
+
+var student = { id:121, phone:1528, name:"Abir"}
+var student2 = { id:142, phone:1646, name:"mahi"}
+
+var phoneNo = student.phone;
+console.log(phoneNo);
+console.log(student2);
