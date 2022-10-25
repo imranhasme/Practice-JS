0e2280f8e1950a747bd91d3dd46a98367e375765
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-25 18:31:41 +0600

    simple function example

---
+++ function.js
@@ -0,0 +1,29 @@
+//return and parameter=========call in function
+
+
+function doubleIt(num){
+    var result = num * 2;
+    return result;
+}
+
+var first = doubleIt(15)
+var secound = doubleIt(49)
+var total = first + secound ;
+// console.log(total);
+
+
+
+
+
+
+
+
+//other function example===========
+ function addSum(num1, num2){
+    var result = num1 + num2;
+    return result;
+
+ }
+
+ var sum = addSum(50, 17);
+ console.log(sum)
