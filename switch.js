606247a7d1bbcc9f36b2511e26fd4d87b436ad20
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-25 17:23:53 +0600

    Switch Case break and default committed

---
+++ switch.js
@@ -0,0 +1,21 @@
+
+//switch case break and default=-------------
+
+num = 10
+switch(num){
+    case 1000:
+        console.log('I am an 1000');
+    break;
+    case 100:
+        console.log('I am an 100')
+    break;
+    case 20:
+    case 10:
+        console.log('I am either 10 or 20')
+        break;
+    default:
+        console.log("I don't know who you are")
+
+}
+
+
