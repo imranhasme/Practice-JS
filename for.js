a372061f5176031a3ea92796acdd9404fa7c7154
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-25 17:10:34 +0600

    for loop example

--- for.js
+++ for.js
@@ -1,7 +1,25 @@
-//while and for loop are same but 
+
+
+//simple for loop---------
+
+
 
 for( var i = 0; i <= 10; i++){
-    console.log(i)
+   console.log(i)
 
 
+ }
+
+
+ //complex example----------
+
+var nums = [48, 22, 99, 39, 40, 83, 62, 75];
+
+for( var i = 0; i < nums.length; i++){
+
+    var element = nums[i];
+    console.log(element)
 }
+
+
+
