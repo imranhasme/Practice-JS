6f31a96210a77522dd95b518d6ff5ec472c8f3dc
imranhasme1wdc <imranhasmewdc.jm@gmail.com>
2022-10-25 21:17:51 +0600

    object final commit

--- object.js
+++ object.js
@@ -1,8 +1,23 @@
-//simple way to call any object element-----------------
+
 
 var student = { id:121, phone:1528, name:"Abir"}
 var student2 = { id:142, phone:1646, name:"mahi"}
+/**
+simple way to call any object element-----------------
+ 
 
 var phoneNo = student.phone;
-console.log(phoneNo);
+
+
+*/
+var phonePropName = "phone";
+var phoneNo1 = student.phone;
+var phoneNo2 = student["phone"];
+var phoneNo3 = student[phonePropName];
+//update phone number-----------
+student2.phone = 548724;
+console.log(phoneNo1);
 console.log(student2);
+
+
+
